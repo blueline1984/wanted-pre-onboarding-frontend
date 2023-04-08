57de934e471173bbedd75d37918377d0d303fca1
blueline1984 <blueline1318@naver.com>
2023-04-08 21:02:20 +0900

    feature: SignIn Page 로그인 post 요청 구현

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,13 +1,30 @@
 import { useState, useEffect } from "react";
 import Form from "../components/Form/Form";
 import { validateEmail, validatePassword } from "../helper/checkValidation";
+import { baseInstance } from "../api/utils/instance";
 
 const SignInPage = () => {
   const [signInInput, setSignInInput] = useState({ email: "", password: "" });
   const [isValid, setIsValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-  const handleSubmitSignIn = (e) => {
+  const handleSubmitSignIn = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await baseInstance.post("auth/signin", {
+        email: signInInput.email,
+        password: signInInput.password,
+      });
+
+      console.log(response.data.access_token);
+
+      setMessage("Successfully Signed In!");
+      setIsLoading(false);
+    } catch (error) {
+      setMessage(error);
+    }
   };
 
   const handleChangeInput = (e) => {
