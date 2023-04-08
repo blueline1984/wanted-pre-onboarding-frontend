1c37935defcfd26d193a6ad6331e1de83849ca03
blueline1984 <blueline1318@naver.com>
2023-04-08 12:12:13 +0900

    feature: SignIn Page UI 작업

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,18 @@
 import * as S from "./Form.styled";
+import Button from "../Button/Button";
 
-const Form = () => {
+const Form = ({ onSubmit, onButtonClick, buttonText, buttonDataTestId }) => {
   return (
-    <S.Form>
-      <S.Label>
-        <S.Input />
-      </S.Label>
-      <S.Label>
-        <S.Input />
-      </S.Label>
-      <button>가입하기</button>
+    <S.Form onSubmit={onSubmit}>
+      <S.Label htmlFor="email">Email</S.Label>
+      <S.Input id="email" type="email" />
+      <S.Label htmlFor="password">Password</S.Label>
+      <S.Input id="password" type="password" />
+      <Button
+        onButtonClick={onButtonClick}
+        buttonText={buttonText}
+        buttonDataTestId={buttonDataTestId}
+      />
     </S.Form>
   );
 };
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,20 @@
+import Form from "../components/Form/Form";
+
 const SignInPage = () => {
+  const handleSubmitSignIn = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <h1>This is Signin Page</h1>
+      <Form
+        onSubmit={handleSubmitSignIn}
+        onButtonClick={() => {
+          console.log("signin");
+        }}
+        buttonText="Sign In"
+        dataTestId="signin-button"
+      />
     </>
   );
 };
