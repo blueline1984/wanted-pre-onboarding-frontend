933e23e1f3f2dc3d792eeb903f819337c53dfad3
blueline1984 <blueline1318@naver.com>
2023-04-08 12:15:12 +0900

    feature: SignUp Page UI 작업

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,21 @@
+import Form from "../components/Form/Form";
+
 const SignUpPage = () => {
+  const handleSubmitSignUp = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h1>This is Signup Page</h1>
+      <Form
+        onSubmit={handleSubmitSignUp}
+        onButtonClick={() => {
+          console.log("signup");
+        }}
+        buttonText="Sign Up"
+        buttonDataTestId="signup-button"
+      />
     </>
   );
 };
